16181a61b33fd002b17c1272e48359f8b8a77750
¨Fabienne¨ <fabienne@thuer.eu>
2024-03-26 11:41:13 +0100

    slider

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,11 @@ st.write('Hello world')
 
 # streamlit run demo_app.py: run app
 # CTRL+C: quit app
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import math
+import random
+
+st.write('Hi!')
